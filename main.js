37dd1e54fff4d43a2de6d10c127d73ed88aae5ae
maksymyankovych <yankovych1604@gmail.com>
2024-08-12 20:13:10 +0300

    Update ui lending page

--- main.js
+++ main.js
@@ -1,37 +1,26 @@
 // homepage-section8
-const previousHeightOfHomeSec8 = 42;
-const heightOfQuestionOfHomeSec8 = [174, 174, 144, 144, 144];
-
 const questionOfHomeSec8 = document.querySelectorAll('.homepage-section8__question');
 const questionIconOfHomeSec8 = document.querySelectorAll('.homepage-section8__question-icon');
 const questionLineOfHomeSec8 = document.querySelectorAll('.homepage-section8__question-line');
 
 questionOfHomeSec8.forEach((area, index) => {
-    let isAciveOfHomeSec8 = false;
 
     area.addEventListener('click', () => {
+        area.classList.toggle('homepage-section8__question--active');
 
-        if (isAciveOfHomeSec8) {
-            isAciveOfHomeSec8 = false;
-            area.style.height = `${previousHeightOfHomeSec8}px`;
-
-            questionIconOfHomeSec8[index].src = './src/images/homepage-section8-question-icon-right.png';
-            questionIconOfHomeSec8[index].style.filter = 'opacity(1)';
+        if (area.classList.contains('homepage-section8__question--active')) {
+            questionIconOfHomeSec8[index].src = './src/images/homepage-section8-question-icon-left.png';
+            questionIconOfHomeSec8[index].classList.add('homepage-section8__question-icon--active');
 
             if (index < questionLineOfHomeSec8.length) {
-                questionLineOfHomeSec8[index].style.height = '1px';
-                questionLineOfHomeSec8[index].style.filter = 'opacity(0.7)';
+                questionLineOfHomeSec8[index].classList.add('homepage-section8__question-line--active');
             }
         } else {
-            isAciveOfHomeSec8 = true;
-            area.style.height = `${heightOfQuestionOfHomeSec8[index]}px`;
-
-            questionIconOfHomeSec8[index].src = './src/images/homepage-section8-question-icon-left.png';
-            questionIconOfHomeSec8[index].style.filter = 'opacity(0.5)';
+            questionIconOfHomeSec8[index].src = './src/images/homepage-section8-question-icon-right.png';
+            questionIconOfHomeSec8[index].classList.remove('homepage-section8__question-icon--active');
 
             if (index < questionLineOfHomeSec8.length) {
-                questionLineOfHomeSec8[index].style.height = '2px';
-                questionLineOfHomeSec8[index].style.filter = 'opacity(1)';
+                questionLineOfHomeSec8[index].classList.remove('homepage-section8__question-line--active');
             }
         }        
     });
@@ -45,8 +34,38 @@ const leftArrowOfHomeSec9 = document.getElementById('homepage-section9__left-arr
 const rightArrowOfHomeSec9 = document.getElementById('homepage-section9__right-arrow-button');
 const reviewOfHomeSec9 = document.querySelectorAll('.homepage-section9__review-info');
 
+function disableButtonsOfHomeSec9() {
+    leftArrowOfHomeSec9.disabled = true;
+    rightArrowOfHomeSec9.disabled = true;
+
+    leftArrowOfHomeSec9.classList.add('homepage-section9__arrow-button-disabled');
+    rightArrowOfHomeSec9.classList.add('homepage-section9__arrow-button-disabled');
+}
+
+function enableButtonsOfHomeSec9() {
+    leftArrowOfHomeSec9.disabled = false;
+    rightArrowOfHomeSec9.disabled = false;
+
+    leftArrowOfHomeSec9.classList.remove('homepage-section9__arrow-button-disabled');
+    rightArrowOfHomeSec9.classList.remove('homepage-section9__arrow-button-disabled');
+}
+
+reviewOfHomeSec9.forEach(review => {
+    review.addEventListener('animationstart', () => {
+        disableButtonsOfHomeSec9();
+    });
+    review.addEventListener('animationend', () => {
+        enableButtonsOfHomeSec9();
+    });
+});
+
 leftArrowOfHomeSec9.addEventListener('click', () => {
     reviewOfHomeSec9[selectedReviewOfHomeSec9].classList.add('homepage-section9__review-delete');
+    reviewOfHomeSec9[selectedReviewOfHomeSec9].classList.remove('homepage-section9__review-fadeOut');
+
+    if (reviewOfHomeSec9[selectedReviewOfHomeSec9].classList.contains('homepage-section9__review-fadeIn')) {
+        reviewOfHomeSec9[selectedReviewOfHomeSec9].classList.remove('homepage-section9__review-fadeIn');
+    }
 
     selectedReviewOfHomeSec9--;
 
@@ -55,10 +74,16 @@ leftArrowOfHomeSec9.addEventListener('click', () => {
     }
 
     reviewOfHomeSec9[selectedReviewOfHomeSec9].classList.remove('homepage-section9__review-delete');
+    reviewOfHomeSec9[selectedReviewOfHomeSec9].classList.add('homepage-section9__review-fadeOut');
 });
 
 rightArrowOfHomeSec9.addEventListener('click', () => {
     reviewOfHomeSec9[selectedReviewOfHomeSec9].classList.add('homepage-section9__review-delete');
+    reviewOfHomeSec9[selectedReviewOfHomeSec9].classList.remove('homepage-section9__review-fadeIn');
+
+    if (reviewOfHomeSec9[selectedReviewOfHomeSec9].classList.contains('homepage-section9__review-fadeOut')) {
+        reviewOfHomeSec9[selectedReviewOfHomeSec9].classList.remove('homepage-section9__review-fadeOut');
+    }
 
     selectedReviewOfHomeSec9++;
 
@@ -67,21 +92,45 @@ rightArrowOfHomeSec9.addEventListener('click', () => {
     }
 
     reviewOfHomeSec9[selectedReviewOfHomeSec9].classList.remove('homepage-section9__review-delete');
+    reviewOfHomeSec9[selectedReviewOfHomeSec9].classList.add('homepage-section9__review-fadeIn');
 });
 // homepage-section9
 
 // homepage-section10
+let timerIntervalOfHomeSec10;
 let selectedRadioOfHomeSec10 = 0;
 
 const radioOfHomeSec10 = document.querySelectorAll('.homepage-section10__radio');
 const insightsOfHomeSec10 = document.querySelectorAll('.homepage-section10__insights');
 
+function startTimerIntervalOfHomeSec10() {
+    timerIntervalOfHomeSec10 = setInterval(() => {
+        insightsOfHomeSec10[selectedRadioOfHomeSec10].classList.add('homepage-section10__insights-delete');
+        radioOfHomeSec10[selectedRadioOfHomeSec10].checked = false;
+    
+        selectedRadioOfHomeSec10++;
+    
+        if (selectedRadioOfHomeSec10 >= radioOfHomeSec10.length) {
+            selectedRadioOfHomeSec10 = 0;
+        }
+    
+        insightsOfHomeSec10[selectedRadioOfHomeSec10].classList.remove('homepage-section10__insights-delete');
+        radioOfHomeSec10[selectedRadioOfHomeSec10].checked = true;
+    }, 5000);
+}
+
+startTimerIntervalOfHomeSec10();
+
 radioOfHomeSec10.forEach((radio, index) => {
     radio.addEventListener('click', () => {
+        clearInterval(timerIntervalOfHomeSec10);
+
         insightsOfHomeSec10[selectedRadioOfHomeSec10].classList.add('homepage-section10__insights-delete');
         insightsOfHomeSec10[index].classList.remove('homepage-section10__insights-delete');
 
         selectedRadioOfHomeSec10 = index;
+
+        startTimerIntervalOfHomeSec10();
     });
 })
 // homepage-section10
@@ -98,7 +147,7 @@ allImagesOfHomeSec11.forEach((image, index) => {
         image.classList.add('homepage-section11__image-checked');
 
         selectedImageOfHomeSec11 = index;
-        mainImageOfHomeSec11.style.backgroundImage = `url('./src/images/homepage-section11-image${selectedImageOfHomeSec11 + 1}-light.png')`;
+        mainImageOfHomeSec11.style.backgroundImage = `url('./src/images/homepage-section11-image${selectedImageOfHomeSec11 + 1}.png')`;
     });
 })
 // homepage-section11
